8f71992fb2e7b050ca7eec18582191da42428dfc
Def-aki0530 <aki990530@gmail.com>
2021-09-13 15:46:12 +0900

    first commit

---
+++ popup.js
@@ -0,0 +1,68 @@
+let shortUrl;
+
+chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, tabs => {
+	var longUrl = tabs[0].url;
+	if( longUrl.indexOf('https://www.amazon.co.jp/') != -1) {
+		var middleUrl = shortenUrls(longUrl);
+		shortUrl = middleUrl.replace('https://','');
+	} else if( longUrl.indexOf('https://www.') != -1) {
+		shortUrl = longUrl.replace('https://www.','');
+	} else if( longUrl.indexOf('https://') != -1) {
+		shortUrl = longUrl.replace('https://','');
+	} else {
+		shortUrl = longUrl;
+	}
+
+});
+
+document.getElementById("button").onclick = function() {
+	copyUrl(shortUrl);
+	var x = document.getElementById("message");
+    x.innerHTML = "コピー<br>しました";
+
+	setTimeout(() => {
+		x.innerHTML = "";
+	}, 2100);
+};
+
+function shortenUrls(url){
+	var urls = url.replace(/http.?:\/\//g, 'https://').split('https://');
+	urls = urls.filter(url => url !== '');
+	urls = urls.map(url => shortenUrl('https://'+url));
+	urls = urls.join('\n');
+	return urls;
+}
+
+function shortenUrl(url) {
+	const domain = getDomain(url);
+	const asin = getAsin(url);
+	if ((domain === null) || (asin === null)) {
+		return null;
+	} else {
+		return domain + asin;
+	} 
+}
+
+function getDomain(url) {
+	var domain = url.match(/http.?:\/\/(www.)?amazon[\w\.]+\//g);
+	if (domain !== null) {
+		domain = domain[0].replace('www.', '').replace(/\/$/g, '');
+	}  
+	return domain;
+}
+
+function getAsin(url) {
+	var url = url.replace(/\/gp\/product\//g, '/dp/').replace('/ASIN/', '/dp/');
+	var asin = url.match(/\/dp\/[\d\w]+/g);
+	if (asin !== null) {
+		asin = asin[0];
+		if (asin.search(/\/$/g) === -1) {
+		asin += '/'; 
+		}
+	}
+	return asin;
+}
+
+function copyUrl(text){
+	navigator.clipboard.writeText(text);
+}
